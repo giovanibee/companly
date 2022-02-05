0e983f01bb925f2e73369fb21e2c4b4c5d9a9710
Laurie Chan Lam <laurie@lady-pony.com>
2022-02-06 00:19:38 +0100

    fix no render return value linting issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import App from './components/app';
 
-// eslint-disable-next-line react/no-render-return-value
-const render = (Component: any) => ReactDOM.render(<Component />, document.getElementById('root'));
-
-render(hot(App));
+const HotApp = hot(App);
+render(<HotApp />, document.getElementById('root'));
